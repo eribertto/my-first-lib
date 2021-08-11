06faa4779743e688a86d149090ab75f35eb85a0f
eribertt_fedorabox <erimendz@gmail.com>
2021-08-11 13:44:45 +0300

    add code inside lib.rs (not replace ok)

--- src/lib.rs
+++ src/lib.rs
@@ -5,3 +5,8 @@ mod tests {
         assert_eq!(2 + 2, 4);
     }
 }
+
+// add this codes per the book
+pub fn hello_from_lib(message: &str) {
+	println!("Printing Hello {} from library", message);
+}
